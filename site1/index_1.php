cbcd4abc70f17534f7e60f36d7820a1c50ed30aa
DROTIK-pyt <55201022+DROTIK-pyt@users.noreply.github.com>
2019-12-09 22:44:39 +0300

    Update index_1.php

--- site1/index_1.php
+++ site1/index_1.php
@@ -8,9 +8,194 @@
 		<title>Главная</title>
 	</head>
 	<body>		
-		<?php require 'header.php' ?>
-		<?php require 'main.php' ?>
-		<?php require 'footer.php' ?>
+	<header>
+		<div class="logo">
+			<div class="conteiner">
+				<img src="image/logo.png" alt="" class="logo-pic"> <!-- width: 200px; height: 48px;-->
+				<ul>
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">Продукция</a></li>
+					<li><a href="#">Покупаем</a></li>
+					<li><a href="#">Услуги</a></li>
+					<li><a href="#">О нас</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="conteiner">
+			<div class="slider">
+				<div class="item">
+					<ul>
+						<li>Собственное производство</li>
+						<li>Покупка новых Б/У поддонов</li>
+						<li>Ремонт и восстановление поддонов<br> любой степени поломки</li>
+					</ul>
+					<div class="img">
+						<img src="image/poddon.png" alt="">
+						<img src="image/pallets.png" alt="">
+					</div>
+				</div>
+				<button class="one">Узнать больше</button>
+				<div class="item">
+					<ul>
+						<li>Может иметь разные размеры<br> и объем</li>
+						<li>Доступная цена</li>
+						<li>Индивидуальный подход к каждому<br> клиенту</li>
+					</ul>
+					<div class="img">
+						<img src="image/doski.png" alt="">
+					</div>
+				</div>
+				<button class="two">Узнать больше</button>
+				<div class="item">
+					<ul>
+						<li>Экологически чистое сырье</li>
+						<li>Обеспечивает больше тепла от<br> сгорания того же объема, что и дров</li>
+						<li>Простота хранения сырья</li>
+						<li>Высокий КПД котла в пределах 96%</li>
+					</ul>
+					<div class="img">
+						<img src="image/1images.png" alt="">
+						<img src="image/ogni.png" alt="">
+					</div>
+				</div>
+				<button class="three">Узнать больше</button>
+				<div class="item">
+					<ul>
+						<li>Широкий выбор контейнеров<br> различных модификаций</li>
+						<li>Простые в эксплуатации, не требуют<br> специального ухода</li>
+						<li>Удобное решение для хранения фруктов<br> и овощей в холодильных камерах</li>
+					</ul>
+					<div class="img">
+						<img src="image/1kont.png" alt="">
+						<img src="image/kapusta.png" alt="">
+						<img src="image/kartoshka.png" alt="">
+					</div>
+				</div>
+				<button class="four">Узнать больше</button>
+			</div>
+			<div class="dots">
+				<div class="dot active"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+				<div class="dot"></div>
+			</div>
+			<div class="footer">
+				<ul>
+					<li><p>Оптимальные цены</p></li>
+					<li><p>Качественное сырье</p></li>
+					<li><p>Производим большие<br> объёмы</p></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+	<main>
+		<div class="conteiner">
+			<div class="title">
+				<h2>Каталог нашей продукции</h2>
+			</div>
+			<div class="products">
+				<div class="product">
+					<img src="image/tovar1.png" alt="">
+					<h3>Поддоны</h3>
+					<div class="discription">
+						<p>Предлагаем новые и бывшие в употреблении поддоны, а также пластиковые поддоны любых видов и размеров.</p>
+					</div>
+					<button><a href="#">Подробнее...</a></button>
+				</div>
+				<div class="product">
+					<img src="image/tovar2.png" alt="">
+					<h3>Топливная паллета</h3>
+					<div class="discription">
+						<p>На древесных паллетах, может использоваться в качестве материала для топлива в промышленной кипятильной системы, гидроэлектростнации и бытовой.</p>
+					</div>
+					<button><a href="#">Подробнее...</a></button>
+				</div>
+				<div class="product">
+					<img src="image/tovar3.png" alt="">
+					<h3>Деревянные контейнеры</h3>
+					<div class="discription">
+						<p>Деревянные, пластиковые и сборные контейнеры для хранения и транспортировки фруктов и овощей.</p>
+					</div>
+					<button><a href="#">Подробнее...</a></button>
+				</div>
+				<div class="product">
+					<img src="image/tovar4.png" alt="">
+					<h3>Тарная дощечка</h3>
+					<div class="discription">
+						<p>Новый вид деревянной тары на рынке.<br> Представляет широкикй спектр возможностей ддял использования при транспортироки, хранении и выклдки товаров.</p>
+					</div>
+					<button><a href="#">Подробнее...</a></button>
+				</div>
+			</div>
+			<div class="services">
+				<div class="conteiner">
+					<div class="title">
+						<h2>Наши услуги</h2>
+					</div>
+					<div class="service">
+						<div class="img">
+							<img src="image/treidin.png" alt="">
+						</div>
+						<div class="discr">
+							<h3>Трейд ин</h3>
+							<p>Мы скупаем все и сразу. Стоимость паллет договорная – в зависимости от количества, состояния и условий доставки. В случае отсутствия возможности доставить поддоны на наш склад, мы готовы предоставить Вам наши услуги по  транспортировке поддонов с Вашей территории. Оплата производится  при отгрузке в удобной для вас форме</p>
+							<button>Подробнее &gt;</button>	
+						</div>
+					</div>	
+					<div class="service">							
+						<div class="discr">
+							<h3>Ремонт поддонов</h3>
+							<p>Мы быстро починим их или обменяем на целые. Современное оборудование и квалифицированный персонал позволяет выполнять ремонт и восстанавливать поддоны  любой степени поломки</p>
+							<button>Подробнее &gt;</button>
+						</div>
+						<div class="img">
+							<img src="image/инструменты.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<div class="head-footer">
+		<p>Все права защищены 2019&copy;</p>
+		<p>&#128386; example@mail.ru</p>
+		<p>&#128383; +7 (900) 123-45-67</p>
+		<i class="fab fa-instagram"></i>
+		<i class="fab fa-vk"></i>
+		<i class="fab fa-youtube"></i>
+	</div>
+	<footer>			
+		<div class="main">
+			<img src="image/lokacion.png" alt="">
+			<form>
+				<h3>Свяжитесь с нами</h3>
+				<input type="text" placeholder="Имя и фамилия">
+				<input type="text" placeholder="Ваша компания">
+				<input type="text" placeholder="Телефон">
+				<input type="text" placeholder="E-mail">
+				<textarea placeholder="Ваш вопрос"></textarea>
+				<input type="submit" value="Отправить">
+			</form>
+		</div>
+		<div class="footer">
+			<div class="info">
+				<h4>Контактная информация</h4>
+				<p>информация</p>
+				<p>информация</p>
+				<p>информация</p>
+				<p>информация</p>
+			</div>
+			<div class="requisites">
+				<h4>Реквизиты</h4>
+				<p>Реквизиты</p>
+				<p>Реквизиты</p>
+				<p>Реквизиты</p>
+				<p>Реквизиты</p>
+				<p>Реквизиты</p>
+				<p>Реквизиты</p>
+			</div>
+		</div>
+	</footer>
 
 		<script src="dist/js/bund.js"></script>
 		<script src="https://kit.fontawesome.com/e4258896cc.js" crossorigin="anonymous"></script>
